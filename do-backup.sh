4df778ff4af0c10f0eb35b97552781731c31c863
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-11 14:53:31 +0200

    Fix dump file name

--- do-backup.sh
+++ do-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.sql`
+DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.archive`
 
 echo -n "Dumping database to ${DUMP_FILE}..."
 /usr/bin/mongodump --archive --host $DB_HOST --db $DB_NAME > ${DUMP_FILE}
